f5888264f3867310c15985d9973f987b419e299f
John Juvi De Villa <69500622+TheSciCoder3000@users.noreply.github.com>
2022-09-28 05:58:48 +0000

    replit commit

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-from configparser import ConfigParser
+# from configparser import ConfigParser
 
-config = ConfigParser()
-config.read('config.ini')
-config_data = config['GENERAL']
+# config = ConfigParser()
+# config.read('config.ini')
+# config_data = config['GENERAL']
+import os
 
-token = config_data['token']
-test_guild = config_data['test_guild']
-tropa_guild = config_data['tropa_guild']
-research_guild = config_data['research_guild']
+token = os.environ['token']
+test_guild = os.environ['test_guild']
+tropa_guild = os.environ['tropa_guild']
+research_guild = os.environ['research_guild']
