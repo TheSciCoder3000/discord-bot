73da5b95096be2c68bd7ad688f34f263f7a54826
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-10-22 14:59:50 +0800

    main bot script code refactoring

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from os import system
 
 class MyBot(commands.Bot):
     def __init__(self):
+        # initialize bot with intents
         super().__init__(intents=discord.Intents.default(), command_prefix="$")
         
+        # load bot cogs extension
         self.initial_extensions = [
             "cogs.assessment.assessment",
             "cogs.subject.subject",
@@ -17,24 +19,24 @@ class MyBot(commands.Bot):
             "cogs.classes.classes"
         ]
 
+    # override setup_hook to load extensions and sync commands across servers
     async def setup_hook(self):
         for ext in self.initial_extensions:
             await self.load_extension(ext)
             
+        # only individually sync bot with specified servers if in dev mode
         if not repl:
             await bot.tree.sync(guild=discord.Object(id=test_guild))
             await bot.tree.sync(guild=discord.Object(id=tropa_guild))
             await bot.tree.sync(guild=discord.Object(id=research_guild))
     
+    # overried on_ready event to start bot scheduler
     async def on_ready(self):
         scheduler.start()
         print(f"Bot logged in as {self.user}")
 
-        if not repl:
-            system('echo "discord bot has been initialized" | festival --tts')
-
-if repl:
-    keepAlive()
+        # if not repl:
+        #     system('echo "discord bot has been initialized" | festival --tts')
 
 bot = MyBot()    
 
@@ -43,14 +45,16 @@ async def remind_me(msg: str, channel_id: int = None, user_id: int = None):
     medium = await bot.fetch_channel(channel_id) if user_id is None else await bot.fetch_user(user_id)
     await medium.send(msg)
 
-def run_bot():
+
+if __name__ == '__main__':
     try:
+        # on production mode, run keepAlive script to prevent forcefully closing the program
+        if repl:
+            keepAlive()
+
         bot.run(token)
     except discord.errors.HTTPException:
         if repl:
             print("\n\n\nBLOCKED BY RATE LIMIT\nRESTARTING NOW\n\n\n")
             system("python restarter.py")
             system("kill 1")
-
-if __name__ == '__main__':
-    run_bot()
